6e9476ab6ae69f74d6d4485b7c2357eb1a9f0195
Fabien Piuzzi <reefab@demenzia.net>
2013-02-20 00:43:44 +0100

    Print updates,
     * doesn't display the "Download this PDF link in the PDF"
     * Put some print specific styles in its own file
     * better status display and error handling in the PDF generator

--- makepdf.rb
+++ makepdf.rb
@@ -4,16 +4,22 @@ module PdfMaker
     class << self
         def registered(app)
             app.after_build do |builder|
-                kit = PDFKit.new(File.new('build/index.html'),
-                            :margin_top => 0,
-                            :margin_bottom => 0,
-                            :margin_left => 0,
-                            :margin_right => 0,
-                            :disable_smart_shrinking => true,
-                            :dpi => 96)
+                begin
+                    kit = PDFKit.new(File.new('build/index.html'),
+                                :margin_top => 0,
+                                :margin_bottom => 0,
+                                :margin_left => 0,
+                                :margin_right => 0,
+                                :disable_smart_shrinking => true,
+                                :dpi => 96,
+                                :print_media_type => true)
 
-                file = kit.to_file('build/resume.pdf')
-                puts "PDF file written"
+                    file = kit.to_file('build/resume.pdf')
+                rescue Exception =>e
+                    builder.say_status "PDF Maker",  "Error: #{e.message}", Thor::Shell::Color::RED
+                    Raise 
+                end
+                builder.say_status "PDF Maker",  "PDF file available at build/resume.pdf"
             end
 
 
